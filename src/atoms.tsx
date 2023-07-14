1bb7675653489c1ac6fc5baf6b8fd297b68fc920
KanuBang <102508014+KanuBang@users.noreply.github.com>
2023-07-14 18:09:29 +0900

    FEAT: Store to LocalStorage

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,5 @@
 import {atom, selector} from "recoil";
+import { recoilPersist } from "recoil-persist";
 
 export interface IToDo {
   id:number;
@@ -10,13 +11,21 @@ interface IToDoSate {
 }
 // 단순 string[]이 아니라 조금 더 구체적으로 됨
 
+
+const {persistAtom}  = recoilPersist({
+  key: "원하는 값",
+  storage:localStorage,
+})
+
+
 export const toDoState = atom<IToDoSate>({
   key:"toDoState",
   default: {
     "TO DO": [],
     Doing: [],
     Done: [],
-  }
+  },
+  effects_UNSTABLE: [persistAtom]
 })
 
 /*
